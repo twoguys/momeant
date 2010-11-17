423601ac16fe48749d02903de04a2b2b488124ef
David Browning <djbrowning@gmail.com>
2010-11-17 13:15:01 -0500

    Removed heroku_env from repo and ignored it

--- config/heroku_env.rb
+++
@@ -1,3 +0,0 @@
-ENV['S3_KEY']                   = 'AKIAIXWWQRY7PRUWUO3A'
-ENV['S3_SECRET']                = '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'
-ENV['S3_BUCKET']                = 'momeant-djbrowning'
