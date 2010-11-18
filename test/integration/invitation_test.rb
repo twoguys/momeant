aaf15cfc812f9608d6a01ba3f1409d3d564babb8
David Browning <djbrowning@gmail.com>
2010-11-17 22:00:14 -0500

    Tested admin inviting creator with some sexy meta programming

--- test/integration/invitation_test.rb
+++ test/integration/invitation_test.rb
@@ -1,38 +1,40 @@
 require File.expand_path('../test_helper', File.dirname(__FILE__))
 
-Feature "A creator or admin (user types) should be able to invite new creators" do
+Feature "A Creator or Admin should be able to invite new creators" do
 
   in_order_to "have more content creators"
   as_a "creator or admin"
   i_want_to "be able to invite new people to become creators"
   
-  Scenario "Inviting a non-existent user to be a creator by email" do
-    Given "An email address does not exist in the system" do
-      @invitee_email = "bob@example.com"
-      assert User.where(:email => @invitee_email).empty?
-    end
+  ["creator", "admin"].each do |user_type|
+    Scenario "A #{user_type.capitalize} invites a non-existent user to be a creator by email" do
+      Given "An email address does not exist in the system" do
+        @invitee_email = "bob@example.com"
+        assert User.where(:email => @invitee_email).empty?
+      end
     
-    Given "A creator exists" do
-      @creator = Factory(:creator)
-    end
+      Given "A #{user_type.capitalize} exists" do
+        instance_variable_set("@#{user_type}", Factory(user_type.to_sym))
+      end
     
-    given_im_signed_in_as(:creator)
+      given_im_signed_in_as(user_type.to_sym)
     
-    when_i_visit_page(:invite_creator_invitations)
+      when_i_visit_page(:invite_creator_invitations)
     
-    And "I fill out and submit the form" do
-      fill_in "invitation_invitee_email", :with => @invitee_email
-      click_button "Invite Creator"
-    end
+      And "I fill out and submit the form" do
+        fill_in "invitation_invitee_email", :with => @invitee_email
+        click_button "Invite Creator"
+      end
     
-    Then "An email should be sent to the invitee's address" do
-      invite_email = ActionMailer::Base.deliveries.last
-      assert_equal invite_email.subject, "You have been invited to be a Creator on Momeant"
-      assert_equal invite_email.to[0], @invitee_email
-    end
+      Then "An email should be sent to the invitee's address" do
+        invite_email = ActionMailer::Base.deliveries.last
+        assert_equal invite_email.subject, "You have been invited to be a Creator on Momeant"
+        assert_equal invite_email.to[0], @invitee_email
+      end
     
-    And "An invitation should be created" do
-      assert_not_nil Invitation.where(:invitee_email => @invitee_email, :inviter_id => @creator.id).first
+      And "An invitation should be created" do
+        assert_not_nil Invitation.where(:invitee_email => @invitee_email, :inviter_id => instance_variable_get("@#{user_type}").id).first
+      end
     end
   end
   
