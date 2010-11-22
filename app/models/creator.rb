47144e5fe91e4d358395205f97a09f85dda68136
David Browning <djbrowning@gmail.com>
2010-11-22 13:30:04 -0500

    Wrote a quick method to get a Creator's sales (through purchases table)

--- app/models/creator.rb
+++ app/models/creator.rb
@@ -1,3 +1,9 @@
 class Creator < User
   has_many :created_stories, :foreign_key => "user_id", :class_name => "Story"
+  
+  def sales
+    # this may be doable in an assocation but I couldn't think of it right away
+    # also probably a better way to do it via one join instead of two selects
+    Purchase.where("story_id IN (#{self.created_stories.map{|s|s.id}.join(',')})")
+  end
 end
