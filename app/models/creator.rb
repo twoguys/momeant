255da48c04caa1730349f1efc2af7dc25a7a8767
David Browning <djbrowning@gmail.com>
2010-11-29 21:14:23 -0500

    Fixed sales association on Creator

--- app/models/creator.rb
+++ app/models/creator.rb
@@ -1,6 +1,6 @@
 class Creator < User
   has_many :created_stories, :foreign_key => :user_id, :class_name => "Story"
-  has_many :sales, :class_name => "Transaction", :foreign_key => :payee_id
+  has_many :sales, :class_name => "Purchase", :foreign_key => :payee_id
   has_many :line_items, :class_name => "PayPeriodLineItem", :foreign_key => :payee_id
   has_many :payments, :foreign_key => :payee_id
   
