043ab0d28dfb0451326281e5e5922b6ed64b915c
David Browning <djbrowning@gmail.com>
2011-02-01 10:34:03 -0500

    Removed topic assocation to parent as it was breaking the destroy action. Only show published stories on the homepage.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   
   def index
-    @stories = Story.order("created_at DESC")
+    @stories = Story.published.order("created_at DESC")
     if current_user
       @subscribed_to_stories = current_user.recommended_stories_from_people_i_subscribe_to
       @similar_to_bookmarked_stories = current_user.stories_similar_to_my_bookmarks_and_purchases
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,7 +1,7 @@
 class Topic < ActiveRecord::Base
   has_and_belongs_to_many :stories
   
-  belongs_to :topic
+  #belongs_to :topic
   has_many :children, :class_name => "Topic", :foreign_key => :topic_id
   
   validates :name, :presence => true, :uniqueness => true
