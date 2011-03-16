17397380e835724f4316a91ad440fee568a784c5
David Browning <djbrowning@gmail.com>
2011-03-16 11:36:53 -0400

    Still messing with env configs for mailer...

--- config/environment.rb
+++ config/environment.rb
@@ -3,5 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Repo::Application.initialize!
-
-config.action_mailer.default_url_options = { :host => "momeant.com" }
