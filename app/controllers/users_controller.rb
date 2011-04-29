d554ea80f1ae79f57ca4674f538ae08d0aec7f07
David Browning <djbrowning@gmail.com>
2011-04-29 14:35:28 -0400

    Skipped authenticity token verification on spreedly update.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_filter :authenticate_user!, :except => [:show, :billing_updates]
+  skip_before_filter :verify_authenticity_token, :only => :billing_updates
   
   def show
     @user = User.find(params[:id])
