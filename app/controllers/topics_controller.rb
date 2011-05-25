e05c38cd9d28980524552f4c9b3f5f1ea362897e
David Browning <djbrowning@gmail.com>
2011-05-25 12:01:12 -0400

    Added the new piggy bank reward coins top-right seciton to the momeant header.

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -3,7 +3,7 @@ class TopicsController < ApplicationController
   def show
     @topic = Topic.where("UPPER(name) = ?", params[:name].upcase).first
     redirect_to root_path, :notice => "Sorry, that topic doesn't exist" if @topic.nil?
-    @hide_stumble = true
+    @hide_rewards = true
     @hide_momeant = true
   end
   
