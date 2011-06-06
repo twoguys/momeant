f1b2749672524ae3ae35ff10553504fc7d204935
David Browning <djbrowning@gmail.com>
2011-06-06 18:46:14 -0400

    Few fixes to analytics calculations

--- public/javascripts/analytics.js
+++ public/javascripts/analytics.js
@@ -7,15 +7,16 @@ var story_stats_browser = function() {
 	
 	var handle_story_click = function() {
 		$(this).addClass('selected').siblings().removeClass('selected');
+		var story_title = $(this).find('.title-and-date h2').text();
+		$('#title').text(story_title);
 		var story_id = $(this).attr('story_id');
 		if (story_id) {
 			eval('var story_rewards = story_' + story_id + '_rewards');
 			eval('var story_views = story_' + story_id + '_views');
-			log(story_views);
 			chart.series[0].data = story_views;
-			//chart.series[1].data = story_rewards;
+			chart.series[1].data = story_rewards;
 			chart.drawSeries({},0);
-			//chart.drawSeries({},1);
+			chart.drawSeries({},1);
 		}
 		return false;
 	};
