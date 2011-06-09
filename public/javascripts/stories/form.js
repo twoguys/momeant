dba8198124d394b1b449efa5118d455a4341969e
David Browning <djbrowning@gmail.com>
2011-06-09 16:19:15 -0400

    Finished up adding creator style widget to tinymce.

--- public/javascripts/stories/form.js
+++ public/javascripts/stories/form.js
@@ -850,20 +850,20 @@ var story_auto_saver = function() {
 			tinyMCE.init(tiny_mce_config);
 			
 			// load Typekit into the tinyMCE iframe
-			log($element.attr('id'));
-			var editor = tinymce.get($element.attr('id')).getDoc();
-			var script = editor.contentDocument.createElement('script');
-			script.type = "text/javascript";
-			script.src = "http://use.typekit.com/" + typekit_id + ".js";
-			script.onload = function() {
-			    editor.contentDocument.Typekit.load({
-			        loading: function() {
-			            editor.contentDocument.designMode = "on";
-			        }
-			    });
-			}
-			editor.contentDocument.designMode = "off"
-			editor.contents().find('head')[0].appendChild(script);
+			// log($element.attr('id'));
+			// var editor = tinymce.get($element.attr('id')).getDoc();
+			// var script = editor.contentDocument.createElement('script');
+			// script.type = "text/javascript";
+			// script.src = "http://use.typekit.com/" + typekit_id + ".js";
+			// script.onload = function() {
+			//     editor.contentDocument.Typekit.load({
+			//         loading: function() {
+			//             editor.contentDocument.designMode = "on";
+			//         }
+			//     });
+			// }
+			// editor.contentDocument.designMode = "off"
+			// editor.contents().find('head')[0].appendChild(script);
 			
 			if ($element.hasClass('mirrored')) {
 				var $to = $('ul#pages li#page_' + number + ' .' + $element.attr('mirror-to') + ', #page-previews li#preview_' + number + ' .' + $element.attr('mirror-to'));
