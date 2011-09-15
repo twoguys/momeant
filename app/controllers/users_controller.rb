c6d5f582371cfb485750ca896c7a42c62cdcb98e
David Browning <djbrowning@gmail.com>
2011-09-15 14:36:37 -0400

    Added edit profile link to user page. Updated user info tagline width

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
   def update
     @user = current_user
     if @user.update_attributes(params[:user])
-      redirect_to bio_user_path(@user), :notice => "Info updated!"
+      redirect_to user_path(@user), :notice => "Info updated!"
     else
       @nav = "home"
       @sidenav = "profile"
