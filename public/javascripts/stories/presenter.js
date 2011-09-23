73c3c3bb493b5cb8cbfcc6a470de9b4491eccf11
David Browning <djbrowning@gmail.com>
2011-09-23 00:07:15 -0400

    Updated content presenter JS to use Backbone.js. Also updated interim styles on reward modal.

---
+++ public/javascripts/stories/presenter.js
@@ -0,0 +1,180 @@
+$(function() {
+	
+	window.Page = Backbone.Model.extend({
+		visible: false,
+		
+		initialize: function() {
+			this.id = '#page_' + this.get('number');
+		}
+	});
+	
+	window.PageView = Backbone.View.extend({
+		
+		initialize: function() {
+			this.el = $(this.model.id);
+			this.listing = $('#metadata .pages a[page="' + this.model.get('number') + '"]');
+			
+			_.bindAll(this, 'update_visibility');
+			this.model.bind('change:visible', this.update_visibility);
+		},
+		
+		update_visibility: function() {
+			if (this.model.get('visible')) {
+				$(this.el).fadeIn('fast');
+				this.listing.addClass('selected');
+			} else {
+				$(this.el).fadeOut('fast');
+				this.listing.removeClass('selected');
+			}
+		}
+	});
+	
+	window.PageList = Backbone.Collection.extend({
+		model: Page,
+		
+		turn_on_page: function(number) {
+			_(this.models).each(function(page) {
+				if (page.get('number') == number) {
+					page.set({visible: true});
+				} else {
+					page.set({visible: false});
+				}
+			});
+		}
+	});
+	
+	window.Pages = new PageList;
+	
+	window.PresenterView = Backbone.View.extend({
+		
+		el: $('#presenter'),
+		
+		events: {
+			'click #previous-page': 'goto_previous_page',
+			'click #next-page':     'goto_next_page',
+			'click .pages a':       'jump_to_page'
+		},
+		
+		initialize: function() {
+			var page_number = 1;
+			$('#pages li').each(function(page) {
+				var $page_li = $(this);
+				var page = new Page({number: page_number, visible: (page_number == 1)});
+				var page_view = new PageView({model: page});
+				Pages.add(page);
+				page_number += 1;
+			});
+			
+			this.current_page = 1;
+			this.previous_button = this.$('#previous-page');
+			this.next_button = this.$('#next-page');
+			this.$('ul#pages li').swipe({swipe:this.swipe,threshold:0});
+			this.goto_page_in_url_or_default();
+			this.setup_key_bindings();
+		},
+		
+		goto_previous_page: function() {
+			this.goto_page(this.current_page - 1);
+		},
+		
+		goto_next_page: function() {
+			this.goto_page(this.current_page + 1);
+		},
+		
+		goto_page: function(number) {
+			if (number >= 1 && number <= Pages.length && number != this.current_page) {
+				this.current_page = number;
+				Pages.turn_on_page(this.current_page);
+			}
+			
+			if (number == 1) {
+				this.previous_button.fadeOut('fast');
+			} else {
+				this.previous_button.fadeIn('fast');
+			}
+			if (number == Pages.length) {
+				this.next_button.fadeOut('fast');
+			} else {
+				this.next_button.fadeIn('fast');
+			}
+		},
+		
+		swipe: function(event, direction) {
+			if (direction == "left") {
+				this.goto_next_page();
+			} else if (direction == "right") {
+				this.goto_prev_page();
+			}
+		},
+		
+		jump_to_page: function(e) {
+			var $link = $(e.currentTarget);
+			var number = parseInt($link.attr('page'));
+			this.goto_page(number);
+		},
+		
+		goto_page_in_url_or_default: function() {
+			var url = window.location.href;
+			if (url.indexOf('#page') != -1) {
+				var number = parseInt(url.substring(url.indexOf('#page') + 5));
+				this.goto_page(number);
+			} else {
+				this.goto_page(1);
+			}
+		},
+		
+		setup_key_bindings: function() {
+			var presenter = this;
+			$(document).keyup(function(e) {
+			  if (e.keyCode == 39) { presenter.goto_next_page(); } 			// right arrow
+				if (e.keyCode == 37) { presenter.goto_previous_page(); }			// left arrow
+			});
+		}
+	});
+	
+	window.RewardModalView = Backbone.View.extend({
+		
+		el: $('#reward-modal'),
+		
+		events: {
+			'click #reward-modal-tab':  'toggle_modal'
+		},
+		
+		initialize: function() {
+			this.modal_open = false;
+			this.setup_key_bindings();
+		},
+		
+		show_modal: function() {
+			$(this.el).animate({top: 0}, 300);
+			this.modal_open = true;
+		},
+		
+		hide_modal: function() {
+			$(this.el).animate({top: '-80%'}, 300);
+			this.modal_open = false;
+		},
+		
+		toggle_modal: function() {
+			if (this.modal_open) {
+				this.hide_modal();
+			} else {
+				this.show_modal();
+			}
+			return false;
+		},
+
+		setup_key_bindings: function() {
+			var modal = this;
+			$(document).keyup(function(e) {
+			  if (e.keyCode == 27) { modal.toggle_modal(); } 			// escape
+			});
+		}
+	});
+	
+	window.Presenter = new PresenterView;
+	window.RewardModal = new RewardModalView;
+	
+	// TODO: move reward JS into the modal object here
+	
+});
