ec6dc540e5bc422b50ffaef5ab58364e45c497bb
David Browning <djbrowning@gmail.com>
2011-09-29 14:21:55 -0400

    Removed community stuff from users_controller. Tweaked tags slightly (text shadow).

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -62,39 +62,6 @@ class UsersController < ApplicationController
     @nav = "home"
   end
   
-  def community
-    @users = []
-    @nav = "community"
-    
-    #content_ids = get_tags_and_stories
-    #return if content_ids.empty?
-    #@users = User.select("DISTINCT ON(id) users.*").joins("LEFT OUTER JOIN curations ON curations.user_id = users.id").where("curations.story_id IN (#{content_ids.join(',')})").where("curations.type = 'Reward'")
-    
-    @users = User.select("DISTINCT ON(id) users.*").joins("LEFT OUTER JOIN curations ON curations.user_id = users.id").where("curations.type = 'Reward'").where("curations.created_at > '#{30.days.ago}'")
-    @users = @users.sort do |a,b|
-      if a.impact != b.impact
-        b.impact <=> a.impact
-      else
-        b.given_rewards.sum(:amount) <=> a.given_rewards.sum(:amount)
-      end
-    end
-  end
-  
-  def community_creators
-    # Tags -> rewards this week -> rewardees -> top content -> top impacter
-    
-    @users = []
-    @nav = "community"
-    
-    #content_ids = get_tags_and_stories
-    #return if content_ids.empty?
-    #@users = Reward.where("curations.story_id IN (#{content_ids.join(',')})").where("curations.type = 'Reward'").group_by(&:recipient).to_a
-    #@users = @users.sort_by {|array| -array.second.inject(0) {|sum,r| r.amount}}
-    
-    @users = User.select("DISTINCT ON(id) users.*").joins("LEFT OUTER JOIN curations ON curations.recipient_id = users.id").where("curations.type = 'Reward'").where("curations.created_at > '#{30.days.ago}'")
-    @users = @users.sort_by {|u| -u.rewards.this_month.sum(:amount)}
-  end
-  
   def feedback
     if current_user
       FeedbackMailer.give_feedback(params[:comment], current_user).deliver
@@ -119,21 +86,5 @@ class UsersController < ApplicationController
       @page_title = @user.name if @user
       @nav = "home"
       @sidenav = "profile" if current_user.present? && @user == current_user
-    end
-    
-    def get_tags_and_stories
-      if params[:tags].blank?
-        @tags = Story.joins(:curations).where("curations.type = 'Reward'").tag_counts.order("count DESC").limit(20) 
-      else      
-        @tags = Story.joins(:curations).where("curations.type = 'Reward'").tagged_with(params[:tags]).tag_counts.order("count DESC").limit(20).sort do |x, y|
-          if params[:tags].include?(x.name)
-            -1
-          else
-            1
-          end
-        end
-      end
-      
-      return Story.tagged_with(@tags, :any => true).map{|story| story.id}
     end
 end
