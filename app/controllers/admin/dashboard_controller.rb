d808792e04f8cd589e130eb41a51b56f5d057885
David Browning <djbrowning@gmail.com>
2011-10-25 11:51:14 -0400

    Added a date filter to the admin money stats.

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,5 +3,16 @@ class Admin::DashboardController < Admin::BaseController
     @payments = AmazonPayment.paid
     @cashouts_paid = Cashout.paid
     @cashouts_requested = Cashout.requested
+    
+    if params[:after].present?
+      @payments = @payments.where("created_at > '#{params[:after]}'")
+      @cashouts_paid = @cashouts_paid.where("created_at > '#{params[:after]}'")
+      @cashouts_requested = @cashouts_requested.where("created_at > '#{params[:after]}'")
+    end
+    if params[:before].present?
+      @payments = @payments.where("created_at < '#{params[:before]}'")
+      @cashouts_paid = @cashouts_paid.where("created_at < '#{params[:before]}'")
+      @cashouts_requested = @cashouts_requested.where("created_at < '#{params[:before]}'")
+    end
   end
 end
