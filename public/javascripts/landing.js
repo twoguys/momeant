8615908229ede42b9b4b4e6b7d159f00f4bc7298
David Browning <djbrowning@gmail.com>
2011-11-29 10:20:50 -0500

    Tweaked the animation of faces on the landing page. Added location and occupation to the info about users on the landing page.

--- public/javascripts/landing.js
+++ public/javascripts/landing.js
@@ -10,21 +10,18 @@ $(function() {
 	  var new_index = $('#faces').children().index($new_face);
 	  
 	  $current_info.fadeOut(200);
-	  $current_face.animate({opacity:0.5}, 200, function() {
-	    $current_face.find('img').animate({width:'80px',height:'80px'}, 200, function() {
-	      $new_face.animate({opacity:1}, 200, function() {
-	        var new_left = new_index * -107 + 50;
-	        $('#faces').animate({left: new_left + 'px'}, 200);
-	        $new_face.find('img').animate({width:'160px',height:'160px'}, 200, function() {
-	          $new_info.fadeIn(200);
-        	  $current_face.removeClass('selected');
-        	  $new_face.addClass('selected');
-        	  $current_info.removeClass('selected');
-        	  $new_info.addClass('selected');
-      	  });
-        });
-	    });
-	  });
+	  $current_face.animate({opacity:0.5, 'margin-top':'40px', width:'80px', height:'80px'}, 200);
+
+    $new_face.animate({opacity:1, width:'160px', height:'160px', 'margin-top':'0'}, 200);
+    
+    var new_left = new_index * -107 + 50;
+    $('#faces').animate({left: new_left + 'px'}, 200, function() {
+      $new_info.fadeIn(200);
+  	  $current_face.removeClass('selected');
+  	  $new_face.addClass('selected');
+  	  $current_info.removeClass('selected');
+  	  $new_info.addClass('selected');
+    });
 	  
 	  return false;
 	});
