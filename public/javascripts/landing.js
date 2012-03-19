b26e416946e1c3346061b1006bcff47274b6ca33
David Browning <djbrowning@gmail.com>
2012-03-19 12:01:13 -0400

    Updated the landing page to deliver our message, and provide access to the about FAQ before discovering creators.

--- public/javascripts/landing.js
+++ public/javascripts/landing.js
@@ -5,11 +5,38 @@ $(function() {
 		el: $('#main'),
 		
 		events: {
+		  'click #goto-about': 'goto_about',
+		  'click #goto-creators': 'goto_creators',
+		  'click #goto-creators-secondary': 'goto_creators',
 			'click #filter a': 'load_people'
 		},
 		
 		initialize: function() {
 			this.current_category = undefined;
+			
+			var height = $(window).height();
+			$('#landing').css('height',height+'px').find('#message, #goto-creators, #goto-about').fadeIn(1000);
+			
+			_.bindAll(this, 'on_resize');
+  	  $(window).resize(this.on_resize);
+  	  
+  	  $.scrollTo(0);
+		},
+		
+		goto_about: function() {
+		  var height = $(window).height();
+		  $.scrollTo(height, 500, {easing:'easeOutQuart'});
+		  return false;
+		},
+		
+		goto_creators: function() {
+		  $('#goto-creators-secondary').hide();
+      $('#messaging').animate({'margin-left': '-50%'}, 500, 'easeOutQuart', function() {
+		    $('#header').animate({top:0}, 300);
+		    $.scrollTo(0, 300);
+		    $('#about').hide();
+		  });
+		  return false;
 		},
 		
 		load_people: function(event) {
@@ -32,6 +59,11 @@ $(function() {
       });
 
       return false;
+		},
+		
+		on_resize: function() {
+		  var height = $(window).height();
+			$('#landing').css('height',height+'px');
 		}
 		
 	});
