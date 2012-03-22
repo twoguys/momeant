516c6521864113adfd0fc96fda985987953a5d3c
David Browning <djbrowning@gmail.com>
2012-03-22 09:26:04 -0400

    Added people to the list, added the Javsacript interactions, tweaked the messaging styles.

--- public/javascripts/landing.js
+++ public/javascripts/landing.js
@@ -2,129 +2,85 @@ $(function() {
   
   window.DiscoveryView = Backbone.View.extend({
 		
-		el: $('#main'),
+		el: $('body'),
 		
 		events: {
-		  'click #goto-about': 'goto_about',
-		  'click #goto-creators': 'goto_creators',
-		  'click #goto-creators-secondary': 'goto_creators',
-			'click #filter a': 'load_people',
-			'click #people a': 'goto_person'
+			'click #people a': 'person_clicked'
 		},
 		
 		initialize: function() {
 			this.current_category = undefined;
-			this.has_gone_down = false;
-			this.has_gone_right = false;
-			
-			var height = $(window).height();
-			$('#landing').css('height',height+'px').find('#message, #goto-creators, #goto-about').fadeIn(1000);
+			this.$window = $(window);
+			this.window_height = this.$window.height();
+			this.current_person = undefined;
 			
 			_.bindAll(this, 'on_resize');
   	  $(window).resize(this.on_resize);
   	  
   	  _.bindAll(this, 'on_keypress');
   	  $(document).bind('keydown', this.on_keypress);
-  	  
-  	  _.bindAll(this, 'goto_creators');
-  	  _.bindAll(this, 'goto_about');
-  	  _.bindAll(this, 'goto_landing');
-  	  
-  	  if (window.location.href.indexOf('browse') > -1) {
-  	    this.goto_creators();
-  	  }
 		},
 		
 		reset: function() {
-		  $('#loader').hide();
-		  $('#messaging, #creators').css('margin-left',0);
-		  $('#about').show();
-  	  $.scrollTo(0);
+  	  $('#slides').scrollTo(0);
 		},
 		
-		goto_about: function() {
-		  var height = $(window).height();
-		  $.scrollTo({top: height, left:0}, 500, {easing:'easeOutQuart'});
-		  this.has_gone_down = true;
-		  return false;
+		goto_messaging: function() {
+		  $('#slides').scrollTo(0, 500, {easing:'easeOutQuart'});
 		},
 		
-		goto_creators: function() {
-		  $('#goto-creators-secondary').hide();
-      $('#messaging').animate({'margin-left': '-50%'}, 500, 'easeOutQuart', function() {
-		    $.scrollTo(0, 300, {onAfter: function() {
-		      $('#header').animate({top:0}, 300);
-		    }});
-		    $('#about').hide();
-		  });
-		  this.has_gone_right = true;
-		  return false;
-		},
-		
-		goto_landing: function() {
-	    $('#messaging').animate({'margin-left': '0'}, 500, 'easeOutQuart', function() {
-  		  $('#header').animate({top:'-60px'}, 300);
-  		  $.scrollTo(0, 500);
-        $('#about, #goto-creators-secondary').show();
-      });
-      this.has_gone_right = false;
-      this.has_gone_down = false;
-		},
-		
-		load_people: function(event) {
-		  var $link = $(event.currentTarget);
-		  if ($link.hasClass('selected')) {
-		    Discovery.current_category = '';
-		    $link.removeClass('selected');
-		  } else {
-		    $link.addClass('selected').parent().siblings().find('a').removeClass('selected');
-        Discovery.current_category = $link.text();
-		  }
-      
-      $('#choose-a-category:visible').fadeOut(200);
-      var $people = $('#people');
-      if (!$people.is(':visible')) { $people.show(); }
-      $people.addClass('loading');
-      $.get('/people?category=' + Discovery.current_category, function(result) {
-        $('#people ul').html(result);
-        $people.removeClass('loading');
-      });
-
-      return false;
-		},
-		
-		goto_person: function(event) {
+		person_clicked: function(event) {
 		  var $person = $(event.currentTarget);
-		  $('#creators').animate({'margin-left': '-50%'}, 500, 'easeOutQuart', function() {
-		    $('#loader').show();
-		  });
+		  Discovery.goto_person($person);
+		},
+		
+		goto_person: function($person) {
+		  Discovery.current_person = $person;
+		  $person.parent().removeClass('faded').siblings().addClass('faded');
+		  var id = $person.attr('data');
+		  var $person_slide = $('#slides #person-' + id);
+		  $('#slides').scrollTo($person_slide, 500, {easing: 'easeOutQuart'});
+		},
+		
+		next_person: function() {
+		  if (Discovery.current_person == undefined) {
+		    Discovery.goto_person($('#people #list li:first-child a'));
+		  } else {
+		    var $next = Discovery.current_person.parent().next();
+		    if ($next.length == 0) { return false; } // we're on the last person in the list
+  		  Discovery.goto_person($next.find('a'));
+		  }
+		  return false;
+		},
+		
+		prev_person: function() {
+		  if (Discovery.current_person == undefined) {
+		    Discovery.goto_person($('#people #list li:first-child a'));
+		  } else {
+		    var $prev = Discovery.current_person.parent().prev();
+		    if ($prev.length == 0) { // we're on the first person in the list
+  	      Discovery.current_person = undefined;
+		      Discovery.goto_messaging();
+		      $('#people #list li').removeClass('faded');
+		      return false;
+		    }
+  		  Discovery.goto_person($prev.find('a'));
+		  }
+		  return false;
 		},
 		
 		on_resize: function() {
-		  var height = $(window).height();
-			$('#landing').css('height',height+'px');
+		  this.window_height = this.$window.height();
 		},
 		
 		on_keypress: function(event) {
 		  var key = event.charCode ? event.charCode : event.keyCode ? event.keyCode : 0;
   	  switch (key) {
   	    case 40: // down arrow
-  	      if (!this.has_gone_down) {
-  	        this.goto_about();
-    	      event.preventDefault();
-  	      }
+  	      this.next_person();
   	      break;
-        case 39: // right arrow
-          if (!this.has_gone_right) {
-            this.goto_creators();
-            event.preventDefault();
-          }
-          break;
-        case 37: // left arrow
-          if (this.has_gone_right) {
-            this.goto_landing();
-            event.preventDefault();
-          }
+        case 38: // up arrow
+          this.prev_person();
           break;
   	  }
 		}
