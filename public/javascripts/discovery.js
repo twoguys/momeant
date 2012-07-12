720612cc8dd7dd09c9aaca2ad572e7555232353d
David Browning <djbrowning@gmail.com>
2012-07-12 16:13:09 -0400

    A few more design/js tweaks to the discover drop-down to make it look/behave more like a drop-down.

--- public/javascripts/discovery.js
+++ public/javascripts/discovery.js
@@ -11,3 +11,9 @@ $('#current-filter').click(function(event) {
 $('#filters ul').click(function(event) {
   event.stopPropagation();
 });
+
+$('#filters ul a').click(function() {
+  var $link = $(this);
+  $('#current-filter').text($link.text());
+  $('#filters ul').toggle();
+});
