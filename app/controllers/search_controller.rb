84a5d256c6ad294c7c303ed1c1ecde1cd0974b82
David Browning <djbrowning@gmail.com>
2012-08-17 19:02:36 -0400

    Added pagination support to search.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -7,18 +7,15 @@ class SearchController < ApplicationController
     
     @content = Sunspot.search(Story) do
       keywords params[:query]
-      any_of do
-        with :published, true
-        with :published, nil
-      end
+      with :published, true
       order_by :reward_count, :desc
     end
-    @content = @content.results
+    @content = Kaminari.paginate_array(@content.results).page(params[:content_page]).per(6)
     
     @creators = Sunspot.search(User) do
       keywords params[:query]
     end
-    @creators = @creators.results
+    @creators = Kaminari.paginate_array(@creators.results).page(params[:creators_page]).per(6)
     
     render "discovery/index"
   end
